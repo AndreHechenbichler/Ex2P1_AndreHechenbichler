cf5e87bfe8e9c5603add3b537f7f104968b90d25
AndreHechenbichler <147879000+AndreHechenbichler@users.noreply.github.com>
2023-12-15 19:14:25 -0500

    Add files via upload

---
+++ Ex2P1_Andre Hechenbichler/src/ex2p1_andre/hechenbichler/Ex2P1_AndreHechenbichler.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex2p1_andre.hechenbichler;
+//fila 3 silla 1
+//hay miedo al exito :(
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andre
+ */
+public class Ex2P1_AndreHechenbichler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Iniciar aventura");
+    public static ArrayList<Personaje> Party=new ArrayList();
+    public static ArrayList<Personaje> Reserva=new ArrayList();
+    public static ArrayList<Items> Backpack=new ArrayList();
+    public static static leer=new Scanner(System.in);
+        personaje.toString
+    }
+    
+}
---
+++ Ex2P1_Andre Hechenbichler/src/ex2p1_andre/hechenbichler/metodos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex2p1_andre.hechenbichler;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andre
+ */
+public class metodos {
+    public ArrayList<String> items(ArrayList<String> items){
+                
+        return null;
+        
+    }
+    
